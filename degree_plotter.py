e8d9638b1b04924375bbfaeb96c42d7d8210c1aa
Robert Dunn <robertdunn220@gmail.com>
2016-09-27 10:55:43 -0400

    Completed celsius converter program

--- degree_plotter.py
+++ degree_plotter.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plot
 
 def farenheit(celsius):
-    return float((celsius * 1.8) + 32)
+    return (celsius * 1.8) + 32
 
-usr_celsius = float(raw_input("Enter a degree in celsius to convert: "))
+xs = range(0, 30)
+ys = []
 
-xs = usr_celsius
-ys = farenheit(usr_celsius)
+for x in xs:
+    ys.append(farenheit(x))
 
 plot.plot(xs, ys)
 plot.show()
